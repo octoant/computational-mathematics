1a70198659362d2b849f4506b26cd4c42cdc0243
Bobur Zakirov <bobur.octo@gmail.com>
2021-03-13 22:21:15 +0300

    Wrote a javadocs

--- lab2 [newton's method]/main/ru/ifmo/cmath/algebra/EquationSolver.java
+++ lab2 [newton's method]/main/ru/ifmo/cmath/algebra/EquationSolver.java
@@ -1,16 +1,25 @@
 package ru.ifmo.cmath.algebra;
 
 /**
- * Equation solver
+ * Equation solver.
  *
  * @since 13 March, 2021
  * @author Bobur Zakirov
  */
 public interface EquationSolver {
 
+    /**
+     * Solve an equation by Bisection method.
+     */
     Object[] solveByBisection(Function function, double a, double b);
 
+    /**
+     * Solve an equation by Iterative method.
+     */
     Object[] solveByIteration(Function function, double a, double b);
 
+    /**
+     * Set an accuracy.
+     */
     void setAccuracy(double accuracy);
 }
