ef32193de484bae412cfb68886879afb5851cfb8
Bobur Zakirov <bobur.octo@gmail.com>
2021-03-28 01:39:04 +0300

    Lagrange Polynomial Builder

--- lab4 [lagrange-polynomial]/main/ru/ifmo/cmath/interpolation/LagrangePolynomialBuilder.java
+++ lab4 [lagrange-polynomial]/main/ru/ifmo/cmath/interpolation/LagrangePolynomialBuilder.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class LagrangePolynomialBuilder {
     private final Function function;
-    private final String lagrangePolynomial;
+    private final StringBuilder lagrangePolynomial;
     private final List<Double> xData;
     private final List<Double> yData;
 
@@ -14,12 +14,16 @@ public class LagrangePolynomialBuilder {
             throw new IllegalArgumentException("Function can not be null.");
         }
         this.function = function;
-        this.lagrangePolynomial = new String();
+        this.lagrangePolynomial = new StringBuilder();
         this.xData = new ArrayList<>(4);
         this.yData = new ArrayList<>(4);
     }
 
     public LagrangePolynomialBuilder experimentalData(Double... data) {
+        if (data.length < 2) {
+            throw new IllegalArgumentException("Experimental points can not be less than 2.");
+        }
+        /* Calculate an experimental data */
         for (Double point : data) {
             this.xData.add(point);
             this.yData.add(function.apply(point));
@@ -27,7 +31,38 @@ public class LagrangePolynomialBuilder {
         return this;
     }
 
+    private StringBuilder lagrangeMultiplier(int i) {
+        StringBuilder numerator = new StringBuilder();
+        Double dominator = 1.0D;
+
+        for (int j = 0; j < xData.size(); j++) {
+            if (i == j) continue;
+            /* Create a numerator of lagrange multiplier */
+            numerator.append("(x-").append(xData.get(j)).append(")");
+            /* Calculate a dominator value */
+            dominator *= (xData.get(i) - xData.get(j));
+
+            if (-1E-6 < dominator && dominator < 1E-6) {
+                throw new IllegalArgumentException("Too small steps!");
+            }
+        }
+        return numerator.append("/").append(dominator);
+    }
+
     public Function build() {
-        return null;
+        if (xData.isEmpty() || yData.isEmpty()) {
+            throw new IllegalArgumentException("Experimental data is empty!");
+        }
+        for (int i = 0; i < yData.size(); i++) {
+            if (yData.get(i).isNaN() || yData.get(i).isInfinite()) {
+                throw new IllegalArgumentException("Function undefined at x=" + xData.get(i));
+            }
+            /* Create a Lagrange Polynomial */
+            lagrangePolynomial.append("+").append(yData.get(i))
+                    .append("(")
+                    .append(lagrangeMultiplier(i))
+                    .append(")");
+        }
+        return new Function(lagrangePolynomial.toString());
     }
 }
