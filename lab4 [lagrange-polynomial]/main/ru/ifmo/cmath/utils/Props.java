b00e537cf4c6081bd9eb0d0aea13c3f073776795
Bobur Zakirov <bobur.octo@gmail.com>
2021-03-28 23:13:38 +0300

    Path to stylesheet

--- lab4 [lagrange-polynomial]/main/ru/ifmo/cmath/utils/Props.java
+++ lab4 [lagrange-polynomial]/main/ru/ifmo/cmath/utils/Props.java
@@ -1,5 +1,7 @@
 package ru.ifmo.cmath.utils;
 
 public final class Props {
-    public static final String PATH = "lab4 [lagrange-polynomial]/main/ru/ifmo/cmath/experimental-data.properties";
+    public static final String FILE = "lab4 [lagrange-polynomial]/main/ru/ifmo/cmath/experimental-data.properties";
+    /* Stylesheet path */
+    public static final String CSS = "ru/ifmo/cmath/chart-styles.css";
 }
