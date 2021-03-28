67774cb1a2597eeddd671920da6cddbe4559d63e
Bobur Zakirov <bobur.octo@gmail.com>
2021-03-28 23:13:22 +0300

    Added 'minOf' and 'maxOf'

--- lab4 [lagrange-polynomial]/main/ru/ifmo/cmath/utils/Array.java
+++ lab4 [lagrange-polynomial]/main/ru/ifmo/cmath/utils/Array.java
@@ -1,15 +1,18 @@
 package ru.ifmo.cmath.utils;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Array {
     public static Double[] parseDoubleArray(String pattern) {
         if (pattern == null || pattern.isEmpty()) {
-            throw new IllegalArgumentException("Array cannot be empty!");
+            throw new IllegalArgumentException("Array cannot be null!");
         }
         if (pattern.startsWith("[") && pattern.endsWith("]")) {
             String[] items = pattern.substring(1, pattern.length()-1).split("(,)");
             /* Return a new empty Double array */
             if (items.length == 1 && items[0].trim().isEmpty()) {
-                return new Double[] {};
+                return new Double[] { };
             }
             /* Build a new Double array length of item's length */
             Double[] array = new Double[items.length];
@@ -21,5 +24,35 @@ public class Array {
         } else {
             throw new RuntimeException("Invalid format of array!");
         }
+    }
+
+    public static Double minOf(Double[]... arrays) {
+        Double minimum = Double.POSITIVE_INFINITY;
+        for (Double[] array : arrays) {
+            try {
+                /* Calculate a min value of array */
+                Double minValue = Collections.min(Arrays.asList(array));
+                /* Check for MIN value */
+                if (minimum > minValue) {
+                    minimum = minValue;
+                }
+            } catch (RuntimeException ignored) { }
+        }
+        return minimum;
+    }
+
+    public static Double maxOf(Double[]... arrays) {
+        Double maximum = Double.NEGATIVE_INFINITY;
+        for (Double[] array : arrays) {
+            try {
+                /* Calculate a max value of array */
+                Double maxValue = Collections.max(Arrays.asList(array));
+                /* Check for MAX value */
+                if (maximum < maxValue) {
+                    maximum = maxValue;
+                }
+            } catch (RuntimeException ignored) { }
+        }
+        return maximum;
     }
 }
