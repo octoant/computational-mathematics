d0d4ccd5b2c75ef28daa075238d4ac8d7b89e95e
Bobur Zakirov <bobur.octo@gmail.com>
2021-04-11 13:15:13 +0300

    Degree -> order

--- lab6 [chebyshev-method]/main/ru/ifmo/cmath/interpolation/ChebyshevPolynomial.java
+++ lab6 [chebyshev-method]/main/ru/ifmo/cmath/interpolation/ChebyshevPolynomial.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChebyshevPolynomial {
-    private final int degree;
+    private final int order;
 
-    public ChebyshevPolynomial(int degree) {
-        this.degree = degree;
+    public ChebyshevPolynomial(int order) {
+        this.order = order;
     }
 
     public List<Double> nodesAt(double a, double b) {
-        List<Double> nodes = new ArrayList<>(degree);
+        List<Double> nodes = new ArrayList<>(order);
         /*
          * Nodes affine transformation over an arbitrary interval [a,b]
          */
@@ -25,15 +25,15 @@ public class ChebyshevPolynomial {
         /*
          * i-th root of Chebyshev polynomial, where i = 1,2,...,n
          */
-        return Math.cos(Math.PI * (i - 0.5) / degree);
+        return Math.cos(Math.PI * (i - 0.5) / order);
     }
 
     public List<Double> roots() {
-        List<Double> roots = new ArrayList<>(degree);
+        List<Double> roots = new ArrayList<>(order);
         /*
          * Chebyshev polynomial roots at (-1,1)
          */
-        for (int i = 1; i <= degree; i++) {
+        for (int i = 1; i <= order; i++) {
             roots.add(root(i));
         }
         return roots;
